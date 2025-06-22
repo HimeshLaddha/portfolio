cee7e997bfe833f77a072a62bff289a6b5977020
Himesh Laddha <159226325+HimeshLaddha@users.noreply.github.com>
2025-06-22 19:55:36 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Education from './components/Education';
+import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-dark text-white">
+      <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+      <Navbar />
+      <Hero />
+      <About />
+      {/* <Education /> */}
+      <Experience />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
